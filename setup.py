3bf7ec571778009b17227ac9481f470d133537e0
Tarek Galal <tare2.galal@gmail.com>
2014-12-29 01:49:21 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+from setuptools import setup
+
+deps = ["protobuf", "pycrypto"]
+
+setup(
+    name='python-axolotl',
+    version="0.01",
+    install_requires = deps,
+    license='GPLv3 License',
+    author='Tarek Galal',
+    author_email='tare2.galal@gmail.com',
+    description="Python port of libaxolotl-android, originally written by Moxie Marlinspik",
+    platforms='any'
+)
