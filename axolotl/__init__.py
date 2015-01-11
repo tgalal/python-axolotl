18492630b293a3b42205290a265f98f22dde3ace
Tarek Galal <tare2.galal@gmail.com>
2015-01-11 16:14:04 +0100

    Fixed installation problem

--- axolotl/__init__.py
+++ axolotl/__init__.py
@@ -1,16 +1,2 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Tarek Galal"
-
-from .duplicatemessagexception import DuplicateMessageException
-from .identitykey import IdentityKey
-from .identitykeypair import IdentityKeyPair
-from .invalidkeyexception import InvalidKeyException
-from .invalidkeyidexception import InvalidKeyIdException
-from .invalidmessageexception import InvalidMessageException
-from .invalidversionexception import InvalidVersionException
-from .legacymessageexception import LegacyMessageException
-from .nosessionexception import NoSessionException
-from .sessionbuilder import SessionBuilder
-from .sessioncipher import SessionCipher
-from .statekeyexchangeexception import StaleKeyExchangeException
-from .untrustedidentityexception import UntrustedIdentityException
