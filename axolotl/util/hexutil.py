c105b17395ec0751c9f26343427079edf278c836
Tarek Galal <tare2.galal@gmail.com>
2015-01-21 04:45:01 +0100

    Fixed hexutil handling of bytes

--- axolotl/util/hexutil.py
+++ axolotl/util/hexutil.py
@@ -3,7 +3,8 @@ decode_hex = codecs.getdecoder("hex_codec")
 class HexUtil:
     @staticmethod
     def decodeHex(hexString):
-        result = decode_hex(hexString.encode())[0]
+        hexString = hexString.encode() if type(hexString) is str else hexString
+        result = decode_hex(hexString)[0]
         if sys.version_info >= (3,0):
             result = result.decode('latin-1')
         return result
