8e3bbb3e4ada8c95c0948f332dc3667bf98a857a
Justin Mayfield <tooker@gmail.com>
2019-03-20 15:10:56 -0600

    Rename module to libsignal (avoid conflict with stdlib)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='libsignal',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'cryptography',
--- signal/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__version__ = '0.1.42'
-__author__ = 'Tarek Galal'
-__email__ = 'tare2.galal@gmail.com'
-__license__ = 'GPLv3'
-__status__ = 'Production'
