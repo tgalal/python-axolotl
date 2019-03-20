1a26d89d26c4d948e77ac3a8bc3402cf51914d6a
Justin Mayfield <tooker@gmail.com>
2019-03-20 15:17:37 -0600

    Ver up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='libsignal',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'cryptography',
